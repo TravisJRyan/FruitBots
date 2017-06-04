10dfc9a41d29651f417ad5f7baff932b3cbdd97c
TravisJRyan <tryan1996@gmail.com>
2017-06-04 09:40:46 -0500

    Update mybot.js
    Updated bot logic. Created a "getDistance" function and then goes after closest fruit (no longer just linear)

--- mybot.js
+++ mybot.js
@@ -1,82 +1,79 @@
-//global vars
-var needOne, needTwo, needThree, totalOne, totalTwo, totalThree;
-
 function new_game() {
-	/* //set vars for total amount of all 3 fruits
-	totalOne = get_total_item_count(1);
-	totalTwo = get_total_item_count(2);
-	totalThree = get_total_item_count(3);
-	
-	//start the game seeking all 3 fruits
-	needOne = true;
-	needTwo = true;
-	needThree = true; */
 }
 
 function insideBoard(x, y) {
-   //coords must be given in [x,y] format
+   //returns whether or not the spot is inside the board
    if ( (x >= 0) && (x < WIDTH) ) {
       if ( (y >= 0) && (y < HEIGHT) ) {
-         return true;
+         return true; //x and y meets requirements, return true
       }
    }
-
-   return false;
+   return false; //return false otherwise
 }
 
 function needFruit(spot){
-	if(spot > 0){
+	if(spot > 0){ // TODO: MAKE "needFruit" ONLY GO FOR FRUIT STILL WORTH GETTING
 		return true;
 	}//end if
 }//end needFruit
+
+function spacesAway(spotX, spotY){
+	//returns how many spaces away a spot is from the player
+	var myX = get_my_x();
+	var myY = get_my_y();
+	return Math.abs(myX-spotX)+Math.abs(myY-spotY);
+}
+
+function findClosest(){
+	var closest = 999;
+	var closestSpot = [-1][-1];
+	var board = get_board(); //get current board state
+	for(x = 0; x < WIDTH; x++){
+		for(y = 0; y<HEIGHT; y++){ //nested loop to check every square
+			if(insideBoard(x,y)){ //ensure point is inside board
+				if( needFruit(board[x][y]) && (spacesAway(x,y) < closest) ){
+					closest = spacesAway(x,y); //keep track of closest distance
+					closestSpot = [x,y]; //keep track of closest coords
+				}
+			}
+		}//end for
+	}//end for
+	return closestSpot; //return x and y of closest spot after all checks are finished
+}
 
 function make_move() {
 	var board = get_board(); //get var for current board state
 	var mySpot = board[get_my_x()][get_my_y()]; //set mySpot variable
 	
-	if(needFruit(mySpot)){
+	console.log(mySpot);
+	
+	if(needFruit(mySpot)){ //if found a needed fruit, take it
 		return TAKE;
 	}
 	
-	for(i = 0; i<HEIGHT+WIDTH; i++){
-		if( insideBoard(get_my_x()+i,get_my_y())){
-			console.log("Yeah that shit exists to the right "+i);
-			if( needFruit(board[get_my_x()+i][get_my_y()] ) ){
-				return EAST;
-			}//end if
-		}
-		else if( insideBoard(get_my_x()-i,get_my_y())){
-			if( needFruit(board[get_my_x()-i][get_my_y()] ) ){
-				return WEST;
-			}//end if
-		}
-		if( insideBoard(get_my_x(),get_my_y()-i)){
-			if( needFruit(board[get_my_x()][get_my_y()-i] ) ){
-				return NORTH;
-			}//end if
-		}
-		if( insideBoard(get_my_x(),get_my_y()+i)){
-			if( needFruit(board[get_my_x()][get_my_y()+i] ) ){
-				return SOUTH;
-			}//end if
-		}
-	}//end for
+	//get coords of closest needed fruit
+	var closest = findClosest();
+	var closestX = closest[0];
+	var closestY = closest[1];
+	
+	//make move accordingly (prioritize x-movement over y-movement)
+	if(get_my_x() < closestX ){
+		return EAST;
+	}
+	if(get_my_x() > closestX){
+		return WEST;
+	}
+	if(get_my_y() < closestY){
+		return SOUTH;
+	}
+	if(get_my_y() > closestY){
+		return NORTH;
+	}
+	
+	//If something went wrong, pass. This shouldn't ever happen.
+	console.log("Yeah I gotta pass. Something went wrong.");
 	return PASS;
 }
-/*
-function updateNeeds(){
-	//If the majority of an item has already been obtained, there is no need to search for it or pick it up anymore
-	if( get_my_item_count(1) >= totalOne/2 || get_opponent_item_count(1) >= totalOne/2){
-			needOne = false;
-	}//end if
-		if( get_my_item_count(2) >= totalTwo/2 || get_opponent_item_count(2) >= totalTwo/2){
-			needTwo = false;
-	}//end if
-		if( get_my_item_count(3) >= totalThree/2 || get_opponent_item_count(3) >= totalThree/2){ 
-			needThree = false; 
-	}//end if 
-}//end updateNeeds
-*/
 
 
 // Optionally include this function if you'd like to always reset to a 
